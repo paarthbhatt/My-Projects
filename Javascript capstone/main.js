ed3d008c9556037e6875ce58ac96fbbcff6351bb
Parth Bhatt <paarthbhatt37@gmail.com>
2024-10-31 14:54:43 +0530

    git add . gitcommit -m "Initial commit" git push

---
+++ Javascript capstone/main.js
@@ -0,0 +1,57 @@
+//   const todos = [
+//       { 
+//           id: 1,
+//           text: 'Take out trash',
+//           isCompleted: true
+//       },
+//       { 
+//           id: 2,
+//           text: 'Meeting with boss',
+//           isCompleted: true
+//       },
+//       { 
+//           id: 3,
+//           text: 'Dentist appt',
+//           isCompleted: false
+//       }
+//  ];
+
+// For loop
+// for(let i = 0; i <= 10; i++) {
+//     console.log(`For Loop Number: ${i}`);
+// }
+
+// While Loop
+// let i = 0;
+// while(i<10) {
+//     console.log(`While Loop Number: ${i}`);
+//     i++;
+// }
+
+// forEach, map, filter
+
+// todos.forEach(function(todo) {
+//     console.log(todo.text);
+// })
+
+// const todoText = todos.map(function(todo) {
+//     return todo.text;
+// });
+// console.log(todo.text);
+
+// const todoCompleted = todos.filter(function(todo) {
+//     return todo.isCompleted === true;
+// }).map(function(todo) {
+//     return todo.text;
+// })
+// console.log(todoCompleted);
+ 
+// const x = 4;
+
+// if(x === 100) {
+//     console.log('x is 100');
+// } else if(x > 10) {
+//     console.log('x is greater than 10');
+// } else {
+//     console.log('x is less than 10');
+// }
