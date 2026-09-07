c74af967ef0d24d6cbfe47c99834fd22f0567d69
ParthBhatt <paarthbhatt37@gmail.com>
2026-09-07 19:58:50 +0530

    Fix Vercel export syntax in redirect.js

--- hack the agent/api/redirect.js
+++ hack the agent/api/redirect.js
@@ -1,6 +1,7 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   const target = "http://localhost:5000/hackaicon_ethiack_1337_lmao";
 
   res.writeHead(302, { Location: target });
   res.end();
-}
+};
+
